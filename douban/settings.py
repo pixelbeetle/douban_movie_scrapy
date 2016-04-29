48c4f0a2e14a77620f0f4dc369b3ceecb84f0735
lhcalibur <liuhan@hotmail.com>
2016-04-29 13:59:15 +0800

    modified

--- douban/settings.py
+++ douban/settings.py
@@ -55,7 +55,7 @@ DEFAULT_REQUEST_HEADERS = {
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
-    'douban.middlewares.useragent_middleware.RotateUserAgentMiddleware': 500
+    'douban.middlewares.useragent_middleware.RotateUserAgentMiddleware': 500,
     # 'douban.middlewares.MyCustomDownloaderMiddleware': 543,
 }
 
